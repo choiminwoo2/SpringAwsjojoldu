ca0833de2c8ada238e72e802ebb73f04fb855fd9
wjrdjs005 <wjrdjs005@naver.com>
2022-09-10 22:33:22 +0900

    '[Posts저장]TestRestTemplate으로통신테스트'

---
+++ src/main/java/com/jojoldu/webservice/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.jojoldu.webservice.service;
+
+import com.jojoldu.webservice.domain.posts.PostsRepository;
+import com.jojoldu.webservice.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
