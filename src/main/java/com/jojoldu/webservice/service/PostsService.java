8c99723f82297020f52ed10bb9fffad434ea4dbf
wjrdjs005 <wjrdjs005@naver.com>
2022-09-16 23:46:52 +0900

    '[포스트업데이트]서비스추가'

--- src/main/java/com/jojoldu/webservice/service/PostsService.java
+++ src/main/java/com/jojoldu/webservice/service/PostsService.java
@@ -1,7 +1,10 @@
 package com.jojoldu.webservice.service;
 
+import com.jojoldu.webservice.domain.posts.Posts;
 import com.jojoldu.webservice.domain.posts.PostsRepository;
+import com.jojoldu.webservice.dto.PostsResponseDto;
 import com.jojoldu.webservice.dto.PostsSaveRequestDto;
+import com.jojoldu.webservice.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,5 +18,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                                        .orElseThrow(()-> new
+                                                IllegalArgumentException("해당 게시글이 없습니다 id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new
+                        IllegalArgumentException("해당 게시글이 없습니다 ID=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
