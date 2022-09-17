0e677b729be6efe3035ae019d1a4f7d0f1afa7e2
wjrdjs005 <wjrdjs005@naver.com>
2022-09-17 23:43:07 +0900

    '[게시글업데이트]업데이트처리'd

--- src/main/java/com/jojoldu/webservice/web/PostsApiController.java
+++ src/main/java/com/jojoldu/webservice/web/PostsApiController.java
@@ -1,12 +1,12 @@
 package com.jojoldu.webservice.web;
 
 
+import com.jojoldu.webservice.dto.PostsResponseDto;
 import com.jojoldu.webservice.dto.PostsSaveRequestDto;
+import com.jojoldu.webservice.dto.PostsUpdateRequestDto;
 import com.jojoldu.webservice.service.PostsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,5 +17,14 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto postsUpdateRequestDto){
+        return postsService.update(id, postsUpdateRequestDto);
+    }
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
     }
 }
