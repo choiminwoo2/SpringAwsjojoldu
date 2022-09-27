a954a4e55885a0523a11ca7a4c381ec71ec2e79a
wjrdjs005 <wjrdjs005@naver.com>
2022-09-27 22:11:15 +0900

    [Index페이지]컨트롤러수정

--- src/main/java/com/jojoldu/webservice/web/IndexController.java
+++ src/main/java/com/jojoldu/webservice/web/IndexController.java
@@ -1,20 +1,26 @@
 package com.jojoldu.webservice.web;
 
 
+import com.jojoldu.webservice.service.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
     @GetMapping("/")
-    public String index(){
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
     @GetMapping("/posts/save")
-    public String post_save(){
+    public String post_save() {
         return "post-save";
     }
 
